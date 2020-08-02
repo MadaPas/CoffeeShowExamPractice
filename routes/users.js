27140007bbbeaa4382aa91f744373c85fab58260
madapas <mada0193@stud.kea.dk>
2020-08-02 19:29:57 +0200

    users route started

--- routes/users.js
+++ routes/users.js
@@ -11,35 +11,20 @@ const footer = fs.readFileSync('./public/fragments/footer.html', 'utf8');
 
 const homeUser = fs.readFileSync('./public/home/homeUser.html', 'utf8');
 
-router.get('/user', (req, res) => {
+router.get('/user', async (req, res) => {
     if (req.session.signedIn) {
         return res.send(header + navbarUser + homeUser + footer);
     }
     return res.redirect('/login');
 });
 
-router.get('/users/data/:id', async (req, res) => {
-        if (req.session.signedIn) {
-            const userWithCoffees = await User.query().select('username').withGraphFetched('coffees').where('id', req.params.id);
+// getting all users- (id & username)
+router.get('/users', async (req, res) => {
+    const allUsers = await User.query().select('id').select('username');  // .select('password')  --> also with the hashed passwords
+    return res.send({
+        response: allUsers
+    });
 
-            return res.send(userWithCoffees);
-            
-        } else {
-            res.send('Please login to view this page!');
-        }
-        res.end();
-});
-
-router.get('/users/data', async (req, res) => {
-    if (req.session.signedIn) {
-        const allUsersWithCoffees = await User.query().select('username').withGraphFetched('coffees');
-
-        return res.send(allUsersWithCoffees);
-        
-    } else {
-        res.send('Please login to view this page!');
-    }
-    res.end();
 });
 
 module.exports = router;
